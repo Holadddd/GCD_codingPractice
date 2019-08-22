b2292a552f4e6091cecfdf6525d7afcf83312067
TH-W <wu19931221@gmail.com>
2019-08-22 14:50:32 +0800

    set group.leave in closure finish.

---
+++ GCD_codingPractice/HTTPsManager.swift
@@ -0,0 +1,78 @@
+//
+//  HTTPsManager.swift
+//  GCD_codingPractice
+//
+//  Created by wu1221 on 2019/8/22.
+//  Copyright © 2019 wu1221. All rights reserved.
+//
+
+import Foundation
+
+enum Result<T> {
+    
+    case success(T)
+    
+    case failure(Error)
+}
+
+enum STHTTPClientError: Error {
+    
+    case decodeDataFail
+    
+    case clientError(Data)
+    
+    case serverError
+    
+    case unexpectedError
+}
+
+class HttpsManger {
+    
+    static var shared = HttpsManger()
+    
+    func getSpeed(offset: Int = 0,completion:@escaping (Result<Data>) -> Void
+        ) {
+        guard let url = URL(string: "https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=5012e8ba-5ace-4821-8482-ee07c147fd0a&limit=1&offset=\(offset)") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        
+        
+        let task = URLSession.shared.dataTask(with: request) { (data,  response, error) in
+            
+            
+            guard error == nil else {
+                
+                return completion(Result.failure(error!))
+            }
+            // swiftlint:disable force_cast
+            let httpResponse = response as! HTTPURLResponse
+            // swiftlint:enable force_cast
+            let statusCode = httpResponse.statusCode
+            
+            switch statusCode {
+                
+            case 200..<300:
+                
+                completion(Result.success(data!))
+                
+            case 400..<500:
+                
+                completion(Result.failure(STHTTPClientError.clientError(data!)))
+                
+            case 500..<600:
+                
+                completion(Result.failure(STHTTPClientError.serverError))
+                
+            default: return
+                
+                completion(Result.failure(STHTTPClientError.unexpectedError))
+            }
+            
+        }
+        task.resume()
+        
+    }
+    
+}
--- GCD_codingPractice/ViewController.swift
+++ GCD_codingPractice/ViewController.swift
@@ -10,11 +10,114 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var apiRequest: UIButton!
+    
+    @IBOutlet weak var offsetTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        apiRequest.addTarget(self, action: #selector(ViewController.make), for: .touchUpInside)
     }
+    let group: DispatchGroup = DispatchGroup()
+    
+    
+    
+    @objc func make() {
+        
 
-
+        let queue1 = DispatchQueue(label: "queue1", attributes: .concurrent)
+        group.enter()
+        queue1.async(group: group){
+            self.makeRequest(offset: 0)
+        }
+        let queue2 = DispatchQueue(label: "queue2", attributes: .concurrent)
+        group.enter()
+        queue2.async(group: group){
+            self.makeRequest(offset: 10)
+        }
+        let queue3 = DispatchQueue(label: "queue3", attributes: .concurrent)
+        group.enter()
+        queue3.async(group: group){
+            self.makeRequest(offset: 20)
+        }
+        
+        group.notify(queue: DispatchQueue.main) {
+            print("done")
+        }
+        
+    }
+    
+    
+    func makeRequest(offset: Int) {
+        
+        HttpsManger.shared.getSpeed(offset: offset) { [weak self] result in
+            guard let strongSelf = self else { fatalError() }
+            switch result {
+            case .success(let data):
+                strongSelf.parseData(data: data)
+            case .failure(let error):
+                print(error)
+            }
+            self?.group.leave()
+        }
+    }
+    
+    
+    
+    func parseData(data:Data) {
+        
+        let decoder = JSONDecoder()
+        do {
+            let data = try decoder.decode(SpeedDate.self, from: data)
+            print(data.result.results[0].road)
+        } catch {
+            print(error)
+        }
+        
+    }
+    
 }
 
+struct SpeedDate :Codable {
+    var result: SpeedInfo
+    enum Codingkeys: String, CodingKey {
+        case result
+    }
+}
+
+struct SpeedInfo: Codable {
+    var limit: Int
+    var offset: Int
+    var count: Int
+    var sort: String
+    var results: [Detail]
+    enum Codingkeys: String, CodingKey {
+        case limit
+        case offset
+        case count
+        case sort
+        case results
+    }
+}
+
+struct Detail: Codable {
+    var functions: String
+    var area: String
+    var no: String
+    var direction: String
+    var speed_limit: String
+    var location: String
+    var _id: Int
+    var road: String
+    enum Codingkeys: String, CodingKey {
+        case functions
+        case area
+        case no
+        case direction
+        case speed_limit
+        case location
+        case _id
+        case road
+    }
+}
